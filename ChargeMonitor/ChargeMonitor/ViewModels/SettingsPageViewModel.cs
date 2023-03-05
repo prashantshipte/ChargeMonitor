7165a2ae1edd140e560e29cf66c47dba2327b42b
Prashant Shipte <prashantshipte@gmail.com>
2023-03-05 17:42:05 +0100

    Settings Page UI Changes

--- ChargeMonitor/ChargeMonitor/ViewModels/SettingsPageViewModel.cs
+++ ChargeMonitor/ChargeMonitor/ViewModels/SettingsPageViewModel.cs
@@ -2,10 +2,22 @@
 namespace ChargeMonitor.ViewModels
 {
     public partial class SettingsPageViewModel : BaseViewModel
-    {
-        public AsyncRelayCommand GoToMainPageCommand { get; }
-
+    {       
         private readonly ISettingsService settingsService;
+
+        [ObservableProperty]
+        bool pushNotificationsEnabled;
+
+        [ObservableProperty]
+        bool soundEnabled;
+
+        [ObservableProperty]
+        bool vibrationEnabled;
+
+        [ObservableProperty]
+        bool darkModeEnabled;
+
+        public AsyncRelayCommand GoToMainPageCommand { get; }
 
         public SettingsPageViewModel(ISettingsService _settingsService)
         {
@@ -15,11 +27,24 @@ namespace ChargeMonitor.ViewModels
         }
 
         private void LoadSettings()
-        {            
+        {
+            PushNotificationsEnabled = settingsService.Get<bool>(nameof(PushNotificationsEnabled), true);
+            SoundEnabled = settingsService.Get<bool>(nameof(SoundEnabled), true);
+            VibrationEnabled = settingsService.Get<bool>(nameof(VibrationEnabled), true);
+            DarkModeEnabled = settingsService.Get<bool>(nameof(darkModeEnabled), true);
+        }
+
+        private void SaveSettings()
+        {
+            settingsService.Save(nameof(PushNotificationsEnabled), PushNotificationsEnabled);
+            settingsService.Save(nameof(SoundEnabled), SoundEnabled);
+            settingsService.Save(nameof(VibrationEnabled), VibrationEnabled);
+            settingsService.Save(nameof(DarkModeEnabled), DarkModeEnabled);            
         }
 
         private async Task GoToMainPageAsync()
         {
+            SaveSettings();
             await Shell.Current.GoToAsync("..", true);
         }
     }
